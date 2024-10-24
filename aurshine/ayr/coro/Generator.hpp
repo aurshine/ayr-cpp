4c67699fb8f61e558e95956b064381ec19b92c16
aurshine <2901658788@qq.com>
2024-10-24 20:17:30 +0800

    重新实现Generator

--- aurshine/ayr/coro/Generator.hpp
+++ aurshine/ayr/coro/Generator.hpp
@@ -1,78 +1,51 @@
 #ifndef AYR_CORO_GENERATOR_HPP
 #define AYR_CORO_GENERATOR_HPP
 
-#include "Awaiter.hpp"
+#include <ayr/detail/printer.hpp>
+
 
 namespace ayr
 {
 	namespace coro
 	{
 		template<typename T>
-		struct GeneratorIterator : public Object<GeneratorIterator<T>>
-		{
-			using self = GeneratorIterator<T>;
-
-			using co_type = std::coroutine_handle<Promise<T>>;
-
-			using iterator_category = std::forward_iterator_tag;
-
-			using value_type = T;
-
-			using difference_type = std::ptrdiff_t;
-
-			using pointer = value_type*;
-
-			using const_pointer = const value_type*;
-
-			using reference = value_type&;
-
-			using const_reference = const value_type&;
-
-			GeneratorIterator(co_type coroutine) : coro_(coroutine) { ++(*this); }
-
-			GeneratorIterator(const self& other) = delete;
-
-			GeneratorIterator& operator=(const self& other) = delete;
-
-			GeneratorIterator(self&& other) : coro_(other.coro_) { other.coro_ = nullptr; }
-
-			GeneratorIterator& operator++()
-			{
-				if (coro_)
-				{
-					coro_.resume();
-					if (coro_.done()) { coro_.destroy(); coro_ = nullptr; }
-				}
-
-				return *this;
-			}
-
-			reference operator*() { return coro_.promise().result(); }
-
-			const_reference operator*() const { return coro_.promise().result(); }
-
-			pointer operator->() { return &coro_.promise().result(); }
-
-			const_pointer operator->() const { return &coro_.promise().result(); }
-
-			bool __equals__(const self& other) const { return coro_.address() == other.coro_.address(); }
-		private:
-			co_type coro_;
-		};
-
-
-		template<typename T>
-		class Generator : public Awaiter<T>
+		class Generator : public Object<Generator<T>>
 		{
 			using self = Generator<T>;
-
-			using super = Awaiter<T>;
+			static_assert(std::is_default_constructible_v<T>, "Generator requires default constructible result type");
 		public:
-			using co_type = super::co_type;
+			struct promise_type
+			{
+				using co_type = std::coroutine_handle<promise_type>;
 
-			Generator(co_type coro) : super(coro) {}
+				std::suspend_never initial_suspend() const noexcept { return {}; }
 
-			Generator(Generator&& other) : super(std::move(other)) {};
+				std::suspend_always final_suspend() const noexcept { return {}; }
+
+				std::suspend_always yield_value(T value) noexcept
+				{
+					result_ = std::move(value);
+					return {};
+				}
+
+				void return_void() const noexcept {}
+
+				void unhandled_exception() const { throw; }
+
+				self get_return_object() { return self(co_type::from_promise(*this)); }
+
+				T& result() { return result_; }
+
+				const T& result() const { return result_; }
+			private:
+				T result_;
+			};
+
+			using co_type = std::coroutine_handle<promise_type>;
+
+			Generator(co_type coro) : coro_(coro) {}
+
+			Generator(Generator&& other) : coro_(std::move(other)) { other.coro_ = nullptr; };
 
 			Generator(const Generator& other) = delete;
 
@@ -80,13 +53,72 @@ namespace ayr
 
 			Generator& operator=(Generator&& other)
 			{
-				super::operator=(std::move(other));
+				if (this != &other)
+				{
+					if (coro_) coro_.destroy();
+					coro_ = other.coro_;
+					other.coro_ = nullptr;
+				}
 				return *this;
 			}
 
-			GeneratorIterator<T> begin() { return GeneratorIterator<T>(super::coro_); }
+			~Generator() { if (coro_) { coro_.destroy(); coro_ = nullptr; } }
 
-			GeneratorIterator<T> end() { return GeneratorIterator<T>(nullptr); }
+			struct GeneratorIterator : public Object<GeneratorIterator>
+			{
+				using self = GeneratorIterator;
+
+				using iterator_category = std::forward_iterator_tag;
+
+				using value_type = T;
+
+				using difference_type = std::ptrdiff_t;
+
+				using pointer = value_type*;
+
+				using const_pointer = const value_type*;
+
+				using reference = value_type&;
+
+				using const_reference = const value_type&;
+
+				GeneratorIterator(co_type coroutine) : coro_(coroutine) {}
+
+				GeneratorIterator(const self& other) = delete;
+
+				GeneratorIterator& operator=(const self& other) = delete;
+
+				GeneratorIterator(self&& other) : coro_(other.coro_) { other.coro_ = nullptr; }
+
+				GeneratorIterator& operator++()
+				{
+					if (coro_)
+					{
+						coro_.resume();
+						if (coro_.done()) coro_ = nullptr;
+					}
+					return *this;
+				}
+
+				reference operator*() { return coro_.promise().result(); }
+
+				const_reference operator*() const { return coro_.promise().result(); }
+
+				pointer operator->() { return &coro_.promise().result(); }
+
+				const_pointer operator->() const { return &coro_.promise().result(); }
+
+				bool __equals__(const self& other) const { return coro_.address() == other.coro_.address(); }
+			private:
+				co_type coro_;
+			};
+
+			GeneratorIterator begin() { return GeneratorIterator(coro_); }
+
+			GeneratorIterator end() { return GeneratorIterator(nullptr); }
+
+		private:
+			co_type coro_;
 		};
 	}
 }
