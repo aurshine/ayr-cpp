29b919f4a3ce22797cb0b0ad619bf1732a8928eb
aurshine <2901658788@qq.com>
2024-10-28 18:27:39 +0800

    修改fs项目结构

--- aurshine/ayr/fs/Path.hpp
+++ aurshine/ayr/fs/Path.hpp
@@ -1,62 +1,8 @@
-﻿#ifndef AYR_FS_PATH_HPP
-#define AYR_FS_PATH_HPP
+#ifndef AYR_FS_PATH_HPP_
+#define AYR_FS_PATH_HPP_
 
-#include <ayr/detail/Printer.hpp>
-
-#ifdef _WIN32
-
-#include <cstdio>
-#include <Windows.h>
-#include <fileapi.h>
-
-
-namespace ayr
-{
-	namespace fs
-	{
-		def exists(const char* path)
-		{
-			DWORD attributes = GetFileAttributesA(path);
-			return (attributes != INVALID_FILE_ATTRIBUTES);
-		}
-
-
-		def isfile(const char* path)
-		{
-			DWORD attr = GetFileAttributesA(path);
-			return (attr != INVALID_FILE_ATTRIBUTES) && ((attr & FILE_ATTRIBUTE_DIRECTORY) == 0);
-		}
-
-
-		def isdir(const char* path)
-		{
-			DWORD attr = GetFileAttributesA(path);
-			return (attr != INVALID_FILE_ATTRIBUTES) && ((attr & FILE_ATTRIBUTE_DIRECTORY) != 0);
-		}
-
-
-		def mkdir(const char* path)
-		{
-			BOOL state = CreateDirectoryA(path, nullptr);
-
-			switch (state)
-			{
-			case ERROR_ALREADY_EXISTS:
-				RuntimeError(std::format("Directory already exists: {}", path));
-			case ERROR_PATH_NOT_FOUND:
-				FileNotFoundError(std::format("Path not found: {}", path));
-			}
-		}
-
-		def mkdir(const CString& path) { mkdir(path.data()); }
-	}
-}
-#elif __linux__
-
-
-#else
-
-#error "Unsupported platform"
-
+#if defined(_WIN32) || defined(_WIN64)
+#include "win/Path.hpp"
 #endif
-#endif 
+
+#endif // AYR_FS_PATH_HPP_
