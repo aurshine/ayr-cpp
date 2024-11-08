5eb0073147a3167eba86fc8f2211f4f19f88a177
aurshine <2901658788@qq.com>
2024-11-08 19:12:09 +0800

    添加命名空间

--- aurshine/ayr/thread/threadpool.hpp
+++ aurshine/ayr/thread/threadpool.hpp
@@ -14,112 +14,116 @@
 
 namespace ayr
 {
-	struct StopFlag
+	namespace thread
 	{
-		// 不停止
-		static constexpr int8_t STOP_NONE = -1;
 
-		// 立即停止
-		static constexpr int8_t STOP_NOW = 0;
-
-		// 等待所有任务完成后停止
-		static constexpr int8_t STOP_FINISH = 1;
-	};
-
-
-	/*线程池类, 当线程池对象被销毁时, 任务队列的里的任务将会被清空*/
-	class ThreadPool : public Object<ThreadPool>, public NoCopy
-	{
-		using self = ThreadPool;
-
-	public:
-		using PoolTask = std::function<void()>;
-
-		// 线程池构造函数，传入线程池的线程数量
-		ThreadPool(size_t num_pool) :threads(num_pool), stop_flag(StopFlag::STOP_NONE)
+		struct StopFlag
 		{
-			auto work = [this]() {
-				while (true)
-				{
-					std::unique_lock<std::mutex> lock(this->mtx);
-					this->condition.wait(lock, [this]() {
-						return this->check_stop_now() || this->check_stop_finish() || !this->tasks.empty();
-						});
+			// 不停止
+			static constexpr int8_t STOP_NONE = -1;
 
-					if (this->check_stop_now() || this->check_stop_finish())	return;
-					PoolTask task = std::move(this->tasks.front());
-					this->tasks.pop();
+			// 立即停止
+			static constexpr int8_t STOP_NOW = 0;
 
-					lock.unlock();
-					task();
-				}
-				};
-			for (auto& t : this->threads)
-				t = std::thread(work);
-		}
+			// 等待所有任务完成后停止
+			static constexpr int8_t STOP_FINISH = 1;
+		};
 
-		~ThreadPool() { stop(); }
 
-		template<class F, class ...Args>
-		auto push(F&& task, Args&& ...args) -> std::future<std::invoke_result_t<F, Args...>> {
-			using ResultType = std::invoke_result_t<F, Args...>;
+		/*线程池类, 当线程池对象被销毁时, 任务队列的里的任务将会被清空*/
+		class ThreadPool : public Object<ThreadPool>, public NoCopy
+		{
+			using self = ThreadPool;
 
-			auto pkg_task = std::make_shared<std::packaged_task<ResultType()>>(
-				std::bind(std::forward<F>(task), std::forward<Args>(args)...)
-			);
+		public:
+			using PoolTask = std::function<void()>;
 
-			std::future<ResultType> future = pkg_task->get_future();
-
+			// 线程池构造函数，传入线程池的线程数量
+			ThreadPool(size_t num_pool) :threads(num_pool), stop_flag(StopFlag::STOP_NONE)
 			{
-				std::unique_lock<std::mutex> lock(this->mtx);
-				tasks.push([pkg_task]() { (*pkg_task)(); });
+				auto work = [this]() {
+					while (true)
+					{
+						std::unique_lock<std::mutex> lock(this->mtx);
+						this->condition.wait(lock, [this]() {
+							return this->check_stop_now() || this->check_stop_finish() || !this->tasks.empty();
+							});
+
+						if (this->check_stop_now() || this->check_stop_finish())	return;
+						PoolTask task = std::move(this->tasks.front());
+						this->tasks.pop();
+
+						lock.unlock();
+						task();
+					}
+					};
+				for (auto& t : this->threads)
+					t = std::thread(work);
 			}
 
-			condition.notify_one();
-			return future;
-		}
+			~ThreadPool() { stop(); }
 
-		// 立即停止线程池
-		void stop() { set_stop_flag(StopFlag::STOP_NOW); join(); }
+			template<class F, class ...Args>
+			auto push(F&& task, Args&& ...args) -> std::future<std::invoke_result_t<F, Args...>> {
+				using ResultType = std::invoke_result_t<F, Args...>;
 
-		// 等待所有任务完成后停止线程池
-		void wait() { set_stop_flag(StopFlag::STOP_FINISH); join(); }
-	private:
-		void join()
-		{
-			for (auto& t : threads)
-				if (t.joinable())
-					t.join();
-		}
+				auto pkg_task = std::make_shared<std::packaged_task<ResultType()>>(
+					std::bind(std::forward<F>(task), std::forward<Args>(args)...)
+				);
 
-		void set_stop_flag(int8_t flag)
-		{
-			std::unique_lock<std::mutex> lock(mtx);
-			stop_flag = flag;
-			lock.unlock();
-			condition.notify_all();
-		}
+				std::future<ResultType> future = pkg_task->get_future();
 
-		bool check_stop_now() const
-		{
-			return stop_flag == StopFlag::STOP_NOW;
-		}
+				{
+					std::unique_lock<std::mutex> lock(this->mtx);
+					tasks.push([pkg_task]() { (*pkg_task)(); });
+				}
 
-		bool check_stop_finish() const
-		{
-			return stop_flag == StopFlag::STOP_FINISH && tasks.empty();
-		}
+				condition.notify_one();
+				return future;
+			}
 
-		Array<std::thread> threads;
+			// 立即停止线程池
+			void stop() { set_stop_flag(StopFlag::STOP_NOW); join(); }
 
-		std::queue<PoolTask> tasks;
+			// 等待所有任务完成后停止线程池
+			void wait() { set_stop_flag(StopFlag::STOP_FINISH); join(); }
+		private:
+			void join()
+			{
+				for (auto& t : threads)
+					if (t.joinable())
+						t.join();
+			}
 
-		std::mutex mtx;
+			void set_stop_flag(int8_t flag)
+			{
+				std::unique_lock<std::mutex> lock(mtx);
+				stop_flag = flag;
+				lock.unlock();
+				condition.notify_all();
+			}
 
-		std::condition_variable condition;
+			bool check_stop_now() const
+			{
+				return stop_flag == StopFlag::STOP_NOW;
+			}
 
-		int8_t stop_flag;
-	};
+			bool check_stop_finish() const
+			{
+				return stop_flag == StopFlag::STOP_FINISH && tasks.empty();
+			}
+
+			Array<std::thread> threads;
+
+			std::queue<PoolTask> tasks;
+
+			std::mutex mtx;
+
+			std::condition_variable condition;
+
+			int8_t stop_flag;
+		};
+	}
 }
 
 #endif // AYR_THREAD_THREADPOOL_HPP
---
+++ aurshine/ayr/thread/utils.hpp
@@ -0,0 +1,13 @@
+#ifndef AYR_THREAD_UTILS_HPP
+#define AYR_THREAD_UTILS_HPP
+
+#include <thread>
+
+namespace ayr
+{
+	namespace thread
+	{
+
+	}
+}
+#endif
