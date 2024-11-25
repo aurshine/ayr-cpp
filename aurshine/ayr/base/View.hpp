179e11c476f0e1f6bb428a72a6b80c53e7e56d54
aurshine <2901658788@qq.com>
2024-11-25 22:57:43 +0800

    增加轻量级视图

--- aurshine/ayr/base/View.hpp
+++ aurshine/ayr/base/View.hpp
@@ -10,17 +10,13 @@ namespace ayr
 	{
 		static_assert(std::is_object_v<T>, "T must be an object type");
 
-		T* view_ptr_;
+		const T* view_ptr_;
 	public:
 		template<typename U>
 			requires std::convertible_to<U, T>
-		View(U& view_ref) : view_ptr_(std::addressof(static_cast<T&>(view_ref))) {}
-
-		T& get() { return *view_ptr_; }
+		View(const U& view_ref) : view_ptr_(std::addressof(static_cast<T&>(view_ref))) {}
 
 		const T& get() const { return *view_ptr_; }
-
-		operator T& () { return *view_ptr_; }
 
 		operator const T& () const { return *view_ptr_; }
 	};
