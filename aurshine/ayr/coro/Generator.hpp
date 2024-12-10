74b92993985ee44ee0ffec0ab97a431c7043c667
aurshine <2901658788@qq.com>
2024-12-10 18:39:34 +0800

    重新实现Generator，内部可以支持co_yield和co_return

--- aurshine/ayr/coro/Generator.hpp
+++ aurshine/ayr/coro/Generator.hpp
@@ -4,11 +4,56 @@
 #include "Promise.hpp"
 
 #include "../base/raise_error.hpp"
+#include "../base/IteratorInfo.hpp"
 
 namespace ayr
 {
 	namespace coro
 	{
+		struct GenStatus
+		{
+			using status_type = uint8_t;
+
+			// 当前状态是yield
+			static constexpr status_type YIELD = 0;
+
+			// 当前状态是return
+			static constexpr status_type RETURN = 1;
+
+			// 当前状态已经结束协程
+			static constexpr status_type DONE = 2;
+		};
+
+		template<typename T>
+		struct GenPromise : public PromiseImpl<T>
+		{
+			using self = GenPromise<T>;
+
+			using super = PromiseImpl<T>;
+
+			using co_type = std::coroutine_handle<self>;
+
+			GenPromise() = default;
+
+			std::suspend_never initial_suspend() const noexcept { return {}; }
+
+			std::suspend_always yield_value(T value) noexcept
+			{
+				super::value_ = std::move(value);
+				return {};
+			}
+
+			void return_value(T value) noexcept
+			{
+				super::value_ = std::move(value);
+				has_return = true;
+			}
+
+			co_type get_return_object() { return co_type::from_promise(*this); }
+
+			bool has_return = false;
+		};
+
 		template<typename T>
 		class Generator : public Object<Generator<T>>
 		{
@@ -16,93 +61,83 @@ namespace ayr
 
 			using self = Generator<T>;
 		public:
-			struct promise_type : public PromiseImpl<T>
-			{
-				using super = PromiseImpl<T>;
-
-				using co_type = std::coroutine_handle<promise_type>;
-
-				std::suspend_never initial_suspend() const noexcept { return {}; }
-
-				std::suspend_always yield_value(T value) noexcept
-				{
-					super::value_ = std::move(value);
-					return {};
-				}
-
-				self get_return_object() { return self(co_type::from_promise(*this)); }
-			};
+			using promise_type = GenPromise<T>;
 
 			using co_type = promise_type::co_type;
 
+			co_type coro_;
+
 			Generator(co_type coro) : coro_(coro) {}
 
-			Generator(Generator&& other) : coro_(std::move(other)) { other.coro_ = nullptr; };
+			Generator(self&& other) : coro_(std::move(other)) { other.coro_ = nullptr; }
 
 			~Generator() { if (coro_) { coro_.destroy(); coro_ = nullptr; } }
 
-			Generator& operator=(Generator&& other)
+			self& operator=(self&& other)
 			{
 				if (this == &other) return *this;
-
-				if (coro_) coro_.destroy();
-				coro_ = other.coro_;
-				other.coro_ = nullptr;
-
-				return *this;
+				ayr_destroy(this);
+				return *ayr_construct(this, std::move(other));
 			}
 
-			struct GeneratorIterator : public Object<GeneratorIterator>
+			T& result() noexcept { return coro_.promise().result(); }
+
+			const T& result() const noexcept { return coro_.promise().result(); }
+
+			struct GeneratorIterator : public IteratorInfo<GeneratorIterator, Generator<T>, std::forward_iterator_tag, T>
 			{
 				using self = GeneratorIterator;
 
-				using iterator_category = std::forward_iterator_tag;
+				using super = IteratorInfo<GeneratorIterator, Generator<T>, std::forward_iterator_tag, T>;
 
-				using value_type = T;
+				GeneratorIterator(super::container_type* gen, GenStatus::status_type status = GenStatus::YIELD) : gen_(gen), status_(status) {}
 
-				using difference_type = std::ptrdiff_t;
+				GeneratorIterator(self&& other) : gen_(other.gen_) { other.gen_ = nullptr; }
 
-				using pointer = value_type*;
-
-				using const_pointer = const value_type*;
-
-				using reference = value_type&;
-
-				using const_reference = const value_type&;
-
-				GeneratorIterator(co_type coroutine) : coro_(coroutine) {}
-
-				GeneratorIterator(self&& other) : coro_(other.coro_) { other.coro_ = nullptr; }
-
-				GeneratorIterator& operator++()
+				self& operator++()
 				{
-					if (coro_)
+					switch (status_)
 					{
-						coro_.resume();
-						if (coro_.done()) coro_ = nullptr;
+					case GenStatus::YIELD:
+						yield_next();
+						break;
+					case GenStatus::RETURN:
+						return_next();
+						break;
+					case GenStatus::DONE:
+						RuntimeError("Generator already finished");
 					}
 					return *this;
 				}
 
-				reference operator*() { return coro_.promise().result(); }
+				super::reference operator*() const { return gen_->result(); }
 
-				const_reference operator*() const { return coro_.promise().result(); }
+				super::pointer operator->() const { return &gen_->result(); }
 
-				pointer operator->() { return &coro_.promise().result(); }
-
-				const_pointer operator->() const { return &coro_.promise().result(); }
-
-				bool __equals__(const self& other) const { return coro_.address() == other.coro_.address(); }
+				bool __equals__(const self& other) const { return gen_ == other.gen_ && status_ == other.status_; }
 			private:
-				co_type coro_;
+				// 当前状态是yield, 进入下一个状态
+				void yield_next()
+				{
+					gen_->coro_.resume();
+					if (gen_->coro_.done())
+						if (gen_->coro_.promise().has_return)
+							status_ = GenStatus::RETURN;
+						else
+							status_ = GenStatus::DONE;
+				}
+
+				// 当前状态是return, 进入下一个状态
+				void return_next() { status_ = GenStatus::DONE; }
+
+				super::container_type* gen_;
+
+				GenStatus::status_type status_;
 			};
 
-			GeneratorIterator begin() { return GeneratorIterator(coro_); }
+			GeneratorIterator begin() { return GeneratorIterator(this); }
 
-			GeneratorIterator end() { return GeneratorIterator(nullptr); }
-
-		private:
-			co_type coro_;
+			GeneratorIterator end() { return GeneratorIterator(this, GenStatus::DONE); }
 		};
 	}
 }
