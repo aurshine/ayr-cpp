1a075c190aa0fb8dd85a2286045b095574cf7a59
aurshine <2901658788@qq.com>
2025-01-06 21:53:46 +0800

    实现Node

--- include/ayr/base/Node.hpp
+++ include/ayr/base/Node.hpp
@@ -6,25 +6,6 @@
 
 namespace ayr
 {
-	// Node类型概念约束
-	template<typename T>
-	concept NodeTypeConcept = requires (T a, const T & b, T && c)
-	{
-		{ T() } -> std::same_as<T>;
-		{ a = b } -> std::same_as<T&>;
-		{ T(b) } -> std::same_as<T>;
-		{ a = std::move(c) } -> std::same_as<T&>;
-		{ T(std::move(c)) } -> std::same_as<T>;
-		{ a.next };
-		{ a.value };
-	};
-
-
-	// BiNode类型概念约束
-	template<typename T>
-	concept BiNodeTypeConcept = NodeTypeConcept<T> && requires (T a) { { a.prev }; };
-
-
 	// 简单节点类型，值能前向移动
 	template<typename T>
 	class SimpleNode : public Object<SimpleNode<T>>
@@ -32,44 +13,55 @@ namespace ayr
 	public:
 		using Value_t = T;
 
-		using self = SimpleNode<T>;
+		using self = SimpleNode<Value_t>;
 	public:
-		SimpleNode() : value(), next(nullptr) {}
+		SimpleNode() : value_(), next_(nullptr) {}
 
-		SimpleNode(const T& value) : value(value), next(nullptr) {}
+		SimpleNode(const Value_t& value) : value_(value), next_(nullptr) {}
 
-		SimpleNode(T&& value) : value(std::move(value)), next(nullptr) {}
+		SimpleNode(Value_t&& value) : value_(std::move(value)), next_(nullptr) {}
 
-		SimpleNode(const self& other) : value(other.value), next(nullptr) {}
+		SimpleNode(const self& other) : value_(other.value_), next_(nullptr) {}
 
-		SimpleNode(self&& other) : value(std::move(other.value)), next(other.next) { other.next = nullptr; }
+		SimpleNode(self&& other) : value_(std::move(other.value_)), next_(std::exchange(other.next, nullptr)) {}
 
 		self& operator=(const self& other)
 		{
-			value = other.value;
-			next = nullptr;
-			return *this;
+			ayr_destroy(this);
+			return *ayr_construct(this, other);
 		}
 
 		self& operator=(self&& other)
 		{
-			value = std::move(other.value);
-			next = other.next;
-			other.next = nullptr;
-			return *this;
+			ayr_destroy(this);
+			return *ayr_construct(this, std::move(other));
 		}
 
-		CString __str__() const override
+		// 获取值
+		Value_t& get() { return value_; }
+
+		const Value_t& get() const { return value_; }
+
+		// 判断是否有下一节点
+		bool has_next() const { return next_ != nullptr; }
+
+		// 获取下一节点
+		self* next_node() const { return next_; }
+
+		// 设置并获取下一节点
+		self* next_node(self* node) { return next_ = node; }
+
+		CString __str__() const
 		{
 			std::stringstream stream;
-			stream << "<Node  " << value << ">";
+			stream << "<Node  " << get() << ">";
 
-			return CString(stream.str());
+			return stream.str();
 		}
+	protected:
+		Value_t value_;
 
-		T value;
-
-		self* next;
+		self* next_;
 	};
 
 
@@ -82,44 +74,114 @@ namespace ayr
 
 		using self = BiSimpleNode<T>;
 	public:
-		BiSimpleNode() : value(), prev(nullptr), next(nullptr) {}
+		BiSimpleNode() : value_(), prev_(nullptr), next_(nullptr) {}
 
-		BiSimpleNode(const T& value) : value(value), prev(nullptr), next(nullptr) {}
+		BiSimpleNode(const T& value) : value_(value), prev_(nullptr), next_(nullptr) {}
 
-		BiSimpleNode(T&& value) : value(std::move(value)), prev(nullptr), next(nullptr) {}
+		BiSimpleNode(T&& value) : value_(std::move(value)), prev_(nullptr), next_(nullptr) {}
 
-		BiSimpleNode(const self& other) : value(other.value), prev(nullptr), next(nullptr) {}
+		BiSimpleNode(const self& other) : value_(std::move(other.value_)), prev_(nullptr), next_(nullptr) {}
 
-		BiSimpleNode(self&& other) : value(std::move(other.value)), prev(other.prev), next(other.next) { other.prev = other.next = nullptr; }
+		BiSimpleNode(self&& other) : value_(std::move(other.value_)), prev_(std::exchange(other.prev_, nullptr)), next_(std::exchange(other.next_, nullptr)) {}
 
 		self& operator=(const self& other)
 		{
-			value = other.value;
-			prev = nullptr;
-			next = nullptr;
-			return *this;
+			ayr_destroy(this);
+			return *ayr_construct(this, other);
 		}
 
 		self& operator=(self&& other)
 		{
-			value = std::move(other.value);
-			prev = other.prev;
-			next = other.next;
-			other.prev = other.next = nullptr;
+			ayr_destroy(this);
+			return *ayr_construct(this, std::move(other));
+		}
+
+		Value_t& get() { return value_; }
+
+		const Value_t& get() const { return value_; }
+
+		// 判断是否有前节点
+		bool has_prev() const { return prev_ != nullptr; }
+
+		// 判断是否有后节点
+		bool has_next() const { return next_ != nullptr; }
+
+		// 获取前节点
+		self* prev_node() const { return prev_; }
+
+		// 获取前节点
+		self* prev_node(self* node)
+		{
+			prev_ = node;
+			if (node)
+				node->next_ = this;
+			return prev_;
+		}
+
+		// 获取后节点，如果没有后节点，未定义行为
+		self* next_node() const { return next_; }
+
+		// 获取后节点
+		self* next_node(self* node)
+		{
+			next_ = node;
+			if (node)
+				node->prev_ = this;
+			return next_;
+		}
+
+		CString __str__() const
+		{
+			std::stringstream stream;
+			stream << "<BiNode  " << get() << ">";
+
+			return stream.str();
+		}
+	protected:
+		self* prev_, * next_;
+
+		Value_t value_;
+	};
+
+
+	template<bool IsConst, typename ND>
+	struct NodeIterator : public IteratorInfo<NodeIterator<IsConst, ND>, NonContainer, std::forward_iterator_tag, typename ND::Value_t>
+	{
+		using self = NodeIterator<IsConst, ND>;
+
+		using ItInfo = IteratorInfo<self, NonContainer, std::forward_iterator_tag, typename ND::Value_t>;
+
+		NodeIterator() : node_(nullptr) {}
+
+		NodeIterator(ND* node) : node_(node) {}
+
+		NodeIterator(const self& other) : node_(other.node_) {}
+
+		self& operator=(const self& other)
+		{
+			node_ = other.node_;
 			return *this;
 		}
 
-		CString __str__() const override
+		typename ItInfo::Reference operator*() const
 		{
-			std::stringstream stream;
-			stream << "<BiNode  " << value << ">";
+			if (node_ == nullptr)
+				NullPointerError("Dereference null pointer");
 
-			return CString(stream.str());
+			return node_->get();
 		}
 
-		T value;
+		ND* operator->() const
+		{
+			if (node_ == nullptr)
+				NullPointerError("Dereference null pointer");
 
-		self* prev, * next;
+			return node_;
+		}
+
+		self& operator++() { node_ = node_->next_node(); return *this; }
+	private:
+		ND* node_;
 	};
 }
 
