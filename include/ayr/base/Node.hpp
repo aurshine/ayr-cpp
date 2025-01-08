36a1c46a64f8c2d8d8fb1ff564fc8d57621520f5
aurshine <2901658788@qq.com>
2025-01-08 22:58:41 +0800

    修改Node的实现

--- include/ayr/base/Node.hpp
+++ include/ayr/base/Node.hpp
@@ -1,5 +1,5 @@
 ﻿#ifndef AYR_BASE_NODE_HPP
-#define AYR_DETIAL_NODE_HPP
+#define AYR_BASE_NODE_HPP
 
 #include "raise_error.hpp"
 #include "ayr_traits.hpp"
@@ -9,43 +9,59 @@ namespace ayr
 {
 	// 简单节点类型，值能前向移动
 	template<typename T>
-	class SimpleNode : public Object<SimpleNode<T>>
+	class SimpleNode : public IteratorInfo<SimpleNode<T>, NonContainer, std::forward_iterator_tag, T>
 	{
 	public:
-		using Value_t = T;
+		using self = SimpleNode<T>;
 
-		using self = SimpleNode<Value_t>;
-	public:
-		SimpleNode() : value_(), next_(nullptr) {}
+		using ItInfo = IteratorInfo<self, NonContainer, std::forward_iterator_tag, T>;
 
-		SimpleNode(const Value_t& value) : value_(value), next_(nullptr) {}
+		// 默认构造函数会构造一个空节点，通常不使用
+		constexpr SimpleNode() : value_(nullptr), next_(nullptr) {}
 
-		SimpleNode(Value_t&& value) : value_(std::move(value)), next_(nullptr) {}
+		SimpleNode(typename ItInfo::const_reference value) : value_(ayr_cove_make(value)), next_(none_node()) {}
 
-		SimpleNode(const self& other) : value_(other.value_), next_(nullptr) {}
+		SimpleNode(typename ItInfo::rvalue_reference value) : value_(ayr_cove_make(std::move(value))), next_(none_node()) {}
 
-		SimpleNode(self&& other) : value_(std::move(other.value_)), next_(std::exchange(other.next, nullptr)) {}
+		SimpleNode(const self& other) : value_(other.value_), next_(other.next_) {}
 
-		// 只拷贝值，不拷贝连接的节点
-		self& operator=(const self& other)
+		self& operator=(const self& other) { value_ = other.value_; next_ = other.next_; return *this; }
+
+		typename ItInfo::reference operator*() const { return *value_; }
+
+		typename ItInfo::pointer operator->() const { return value_; }
+
+		self& operator++() { return *this = *next_; }
+
+		self operator++(int) { self tmp(*this); ++*this; return tmp; }
+
+		self operator+(ItInfo::difference_type n) const { return std::next(*this, n); }
+
+		self& operator+=(ItInfo::difference_type n) { std::advance(*this, n); return *this; }
+
+		typename ItInfo::difference_type operator-(const self& other) const { return std::distance(other, *this); }
+
+		bool __equals__(const self& other) const { return value_ == other.value_ && next_ == other.next_; }
+
+		// 空节点
+		constexpr static self* none_node()
 		{
-			ayr_destroy(this);
-			return *ayr_construct(this, other);
+			static self* none_node_ = ayr_make<self>();
+			return none_node_;
 		}
 
-		self& operator=(self&& other)
+		// 释放分配的内存
+		void destroy()
 		{
-			ayr_destroy(this);
-			return *ayr_construct(this, std::move(other));
+			ayr_desloc(value_, 1);
+			std::memset(this, 0, sizeof self);
 		}
 
-		// 获取值
-		Value_t& get() { return value_; }
-
-		const Value_t& get() const { return value_; }
+		// 判断是否是空节点
+		bool is_none_node() const { return value_ == nullptr && next_ == nullptr; }
 
 		// 判断是否有下一节点
-		bool has_next() const { return next_ != nullptr; }
+		bool has_next() const { return !(next_ == nullptr || next_->is_none_node()); }
 
 		// 获取下一节点
 		self* next_node() const { return next_; }
@@ -56,57 +72,91 @@ namespace ayr
 		CString __str__() const
 		{
 			std::stringstream stream;
-			stream << "<Node  " << get() << ">";
+			stream << "<Node  " << **this << ">";
 
 			return stream.str();
 		}
-	protected:
-		Value_t value_;
+	private:
+		typename ItInfo::pointer value_;
 
 		self* next_;
 	};
 
-
 	// 双向节点类型，值能前后移动
 	template<typename T>
-	class BiSimpleNode : public Object<BiSimpleNode<T>>
+	class BiSimpleNode : public IteratorInfo<BiSimpleNode<T>, NonContainer, std::bidirectional_iterator_tag, T>
 	{
 	public:
-		using Value_t = T;
-
 		using self = BiSimpleNode<T>;
-	public:
-		BiSimpleNode() : value_(), prev_(nullptr), next_(nullptr) {}
 
-		BiSimpleNode(const T& value) : value_(value), prev_(nullptr), next_(nullptr) {}
+		using ItInfo = IteratorInfo<self, NonContainer, std::bidirectional_iterator_tag, T>;
 
-		BiSimpleNode(T&& value) : value_(std::move(value)), prev_(nullptr), next_(nullptr) {}
+		// 默认构造函数会构造一个空节点，通常不使用
+		constexpr BiSimpleNode() : value_(nullptr), prev_(nullptr), next_(nullptr) {}
 
-		BiSimpleNode(const self& other) : value_(std::move(other.value_)), prev_(nullptr), next_(nullptr) {}
+		BiSimpleNode(typename ItInfo::const_reference value) : value_(ayr_cove_make(value)), prev_(none_node()), next_(none_node()) {}
 
-		BiSimpleNode(self&& other) : value_(std::move(other.value_)), prev_(std::exchange(other.prev_, nullptr)), next_(std::exchange(other.next_, nullptr)) {}
+		BiSimpleNode(typename ItInfo::rvalue_reference value) : value_(ayr_cove_make(std::move(value))), prev_(none_node()), next_(none_node()) {}
+
+		BiSimpleNode(const self& other) :
+			value_(other.value_),
+			prev_(other.prev_),
+			next_(other.next_) {}
 
 		self& operator=(const self& other)
 		{
-			ayr_destroy(this);
-			return *ayr_construct(this, other);
+			value_ = other.value_;
+			prev_ = other.prev_;
+			next_ = other.next_;
+			return *this;
 		}
 
-		self& operator=(self&& other)
+		typename ItInfo::reference operator*() const { return *value_; }
+
+		typename ItInfo::pointer operator->() const { return value_; }
+
+		self& operator++ () { return *this = *next_; }
+
+		self operator++(int) { self tmp(*this); ++*this; return tmp; }
+
+		self& operator--() { return *this = *prev_; }
+
+		self operator--(int) { self tmp(*this); --*this; return tmp; }
+
+		self operator+(ItInfo::difference_type n) const { return std::next(*this, n); }
+
+		self& operator+=(ItInfo::difference_type n) { std::advance(*this, n); return *this; }
+
+		self operator-(ItInfo::difference_type n) const { return std::prev(*this, n); }
+
+		self& operator-=(ItInfo::difference_type n) { std::advance(*this, -n); return *this; }
+
+		typename ItInfo::difference_type operator-(const self& other) const { return std::distance(other, *this); }
+
+		bool __equals__(const self& other) const { return value_ == other.value_ && prev_ == other.prev_ && next_ == other.next_; }
+
+		// 空节点
+		constexpr static self* none_node()
 		{
-			ayr_destroy(this);
-			return *ayr_construct(this, std::move(other));
+			static self* none_node_ = ayr_make<self>();
+			return none_node_;
 		}
 
-		Value_t& get() { return value_; }
+		// 释放分配的内存
+		void destroy()
+		{
+			ayr_desloc(value_, 1);
+			std::memset(this, 0, sizeof self);
+		}
 
-		const Value_t& get() const { return value_; }
+		// 判断是否是空节点
+		bool is_none_node() const { return value_ == nullptr && prev_ == nullptr && next_ == nullptr; }
 
 		// 判断是否有前节点
-		bool has_prev() const { return prev_ != nullptr; }
+		bool has_prev() const { return !(prev_ == nullptr || prev_->is_none_node()); }
 
 		// 判断是否有后节点
-		bool has_next() const { return next_ != nullptr; }
+		bool has_next() const { return !(next_ == nullptr || next_->is_none_node()); }
 
 		// 获取前节点
 		self* prev_node() const { return prev_; }
@@ -115,7 +165,7 @@ namespace ayr
 		self* prev_node(self* node)
 		{
 			prev_ = node;
-			if (node)
+			if (!node->is_none_node())
 				node->next_ = this;
 			return prev_;
 		}
@@ -127,7 +177,7 @@ namespace ayr
 		self* next_node(self* node)
 		{
 			next_ = node;
-			if (node)
+			if (!node->is_none_node())
 				node->prev_ = this;
 			return next_;
 		}
@@ -135,101 +185,14 @@ namespace ayr
 		CString __str__() const
 		{
 			std::stringstream stream;
-			stream << "<BiNode  " << get() << ">";
+			stream << "<BiNode  " << **this << ">";
 
 			return stream.str();
 		}
-	protected:
-		self* prev_, * next_;
-
-		Value_t value_;
-	};
-
-
-	template<bool IsConst, bool IsBidirectional, typename ND>
-	struct NodeIterator : public IteratorInfo<NodeIterator<IsConst, IsBidirectional, ND>, NonContainer, std::conditional_t<IsBidirectional, std::bidirectional_iterator_tag, std::forward_iterator_tag>, add_const_t<IsConst, typename ND::Value_t>>
-	{
-		using self = NodeIterator<IsConst, IsBidirectional, ND>;
-
-		using ItInfo = IteratorInfo<self, NonContainer, std::conditional_t<IsBidirectional, std::bidirectional_iterator_tag, std::forward_iterator_tag>, add_const_t<IsConst, typename ND::Value_t>>;
-
-		using Node_t = ND;
-
-		NodeIterator() : node_(nullptr) {}
-
-		NodeIterator(Node_t* node) : node_(node) {}
-
-		NodeIterator(const self& other) : node_(other.node_) {}
-
-		self& operator=(const self& other) { node_ = other.node_; return *this; }
-
-		typename ItInfo::reference operator*() const
-		{
-			if (node_ == nullptr)
-				NullPointerError("Dereference null pointer");
-
-			return node_->get();
-		}
-
-		typename ItInfo::pointer operator->() const
-		{
-			if (node_ == nullptr)
-				NullPointerError("Dereference null pointer");
-
-			return &node_->get();
-		}
-
-		self& operator++() { node_ = node_->next_node(); return *this; }
-
-		self operator++(int) { self tmp(*this); ++*this; return tmp; }
-
-		self& operator--() requires IsBidirectional { node_ = node_->prev_node(); return *this; }
-
-		self operator--(int) requires IsBidirectional { self tmp(*this); --*this; return tmp; }
-
-		self operator+(ItInfo::difference_type n) const
-		{
-			Node_t* tmp = node_;
-			while (n--) tmp = tmp->next_node();
-			return tmp;
-		}
-
-		self operator-(ItInfo::difference_type n) const requires IsBidirectional
-		{
-			Node_t* tmp = node_;
-			while (n--) tmp = tmp->prev_node();
-			return tmp;
-		}
-
-		self& operator+=(ItInfo::difference_type n)
-		{
-			while (n--) node_ = node_->next_node();
-			return *this;
-		}
-
-		self& operator-=(ItInfo::difference_type n) requires IsBidirectional
-		{
-			while (n--) node_ = node_->prev_node();
-			return *this;
-		}
-
-		typename ItInfo::difference_type operator-(const self& other) const
-		{
-			Node_t* tmp = node_;
-			typename ItInfo::difference_type n = 0;
-			while (tmp != other.node_)
-			{
-				tmp = tmp->next_node();
-				if (tmp == nullptr)
-					RuntimeError("cannot calculate distance between two iterators");
-				++n;
-			}
-			return n;
-		}
-
-		bool __equals__(const self& other) const { return node_ == other.node_; }
 	private:
-		Node_t* node_;
+		typename ItInfo::pointer value_;
+
+		self* prev_, * next_;
 	};
 }
 
