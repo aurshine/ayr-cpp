189b2a51e2d47957042d5c8851bd0be2abacdbec
aurshine <2901658788@qq.com>
2025-02-23 17:18:45 +0800

    重构RobinManager和RobinBucket

--- include/ayr/base/HashBucket.hpp
+++ include/ayr/base/HashBucket.hpp
@@ -57,8 +57,6 @@ namespace ayr
 	public:
 		using Value_t = T;
 
-		RobinManager() : _move_dist(MOVED_NOTHING), _hashv(0), _value() {}
-
 		RobinManager(const Value_t& value, hash_t hashv, uint32_t move_dist) :
 			_move_dist(move_dist),
 			_hashv(hashv),
@@ -72,10 +70,7 @@ namespace ayr
 		RobinManager(RobinManager&& other) noexcept :
 			_move_dist(other._move_dist),
 			_hashv(other._hashv),
-			_value(std::move(other._value))
-		{
-			other.unmanage();
-		}
+			_value(std::move(other._value)) {}
 
 		self& operator=(const self& other)
 		{
@@ -94,7 +89,6 @@ namespace ayr
 			_move_dist = other._move_dist;
 			_hashv = other._hashv;
 			_value = std::move(other._value);
-			other.unmanage();
 			return *this;
 		}
 
@@ -103,12 +97,6 @@ namespace ayr
 		const Value_t& value() const { return _value; }
 
 		hash_t hashv() const { return _hashv; }
-
-		// 是否被管理
-		bool is_managed() const { return _move_dist != MOVED_NOTHING; }
-
-		// 取消管理
-		void unmanage() { _move_dist = MOVED_NOTHING; }
 
 		bool hash_equal(hash_t hashv) const { return _hashv == hashv; }
 
@@ -119,20 +107,14 @@ namespace ayr
 
 		void add_move_dist(int d = 1) { _move_dist += d; }
 
-		void swap(self& other)
-		{
-			std::swap(_move_dist, other._move_dist);
-			std::swap(_hashv, other._hashv);
-			std::swap(_value, other._value);
-		}
+		// 重置移动距离
+		void reset_move_dist() { _move_dist = 0; }
 	private:
 		int _move_dist;
 
 		hash_t _hashv;
 
 		Value_t _value;
-
-		constexpr static int MOVED_NOTHING = INT_MAX;
 	};
 
 	template<typename T>
@@ -148,26 +130,33 @@ namespace ayr
 
 		RobinHashBucket() : RobinHashBucket(0) {}
 
-		RobinHashBucket(c_size size) : robin_managers_(size) {}
+		RobinHashBucket(c_size size) : robin_managers_(size, nullptr) {}
 
-		RobinHashBucket(const RobinHashBucket& other) : robin_managers_(other.robin_managers_) {}
+		RobinHashBucket(const self& other) : RobinHashBucket(other.robin_managers_.size())
+		{
+			for (int i = 0, n = capacity(); i < n; ++i)
+				if (other.robin_managers_.at(i) != nullptr)
+					robin_managers_.at(i) = ayr_make<Manager_t>(*other.robin_managers_.at(i));
+		}
 
-		RobinHashBucket(RobinHashBucket&& other) noexcept : robin_managers_(std::move(other.robin_managers_)) {}
+		RobinHashBucket(self&& other) noexcept : robin_managers_(std::move(other.robin_managers_)) {}
+
+		~RobinHashBucket() { clear(); }
 
 		self& operator=(const self& other)
 		{
 			if (this == &other) return *this;
 
-			robin_managers_ = other.robin_managers_;
-			return *this;
+			ayr_destroy(this, 1);
+			return *ayr_construct(this, other);
 		}
 
 		self& operator=(self&& other) noexcept
 		{
 			if (this == &other) return *this;
 
-			robin_managers_ = std::move(other.robin_managers_);
-			return *this;
+			ayr_destroy(this, 1);
+			return *ayr_construct(this, std::move(other));
 		}
 
 		c_size capacity() const { return robin_managers_.size(); }
@@ -176,25 +165,25 @@ namespace ayr
 		// 1. 找到相同的hash值, 返回该管理器地址
 		// 2. 找到距离最近的空闲管理器, 返回该管理器地址
 		// 3. 没有空闲且相同hash值的管理器，返回nullptr
-		Manager_t* try_get_manager(hash_t hashv)
+		Manager_t** try_get_manager(hash_t hashv)
 		{
 			c_size capacity_ = capacity();
 			for (c_size index = super::hash2index(hashv), i = 0; i < capacity_; ++i)
 			{
-				Manager_t& manager = robin_managers_.at((index + i) % capacity_);
-				if (!manager.is_managed() || manager.hash_equal(hashv))
+				Manager_t*& manager = robin_managers_.at((index + i) % capacity_);
+				if (manager == nullptr || manager->hash_equal(hashv))
 					return &manager;
 			}
 			return nullptr;
 		}
 
-		const Manager_t* try_get_manager(hash_t hashv) const
+		Manager_t* const* try_get_manager(hash_t hashv) const
 		{
 			c_size capacity_ = capacity();
 			for (c_size index = super::hash2index(hashv), i = 0; i < capacity_; ++i)
 			{
-				const Manager_t& manager = robin_managers_.at((index + i) % capacity_);
-				if (!manager.is_managed() || manager.hash_equal(hashv))
+				Manager_t* const& manager = robin_managers_.at((index + i) % capacity_);
+				if (manager == nullptr || manager->hash_equal(hashv))
 					return &manager;
 			}
 			return nullptr;
@@ -202,18 +191,18 @@ namespace ayr
 
 		Value_t* try_get(hash_t hashv)
 		{
-			Manager_t* manager = try_get_manager(hashv);
-			if (manager == nullptr || !manager->is_managed())
+			Manager_t** manager = try_get_manager(hashv);
+			if (manager == nullptr || *manager == nullptr)
 				return nullptr;
-			return &manager->value();
+			return &((**manager).value());
 		}
 
 		const Value_t* try_get(hash_t hashv) const
 		{
-			const Manager_t* manager = try_get_manager(hashv);
-			if (manager == nullptr || !manager->is_managed())
+			Manager_t* const* manager = try_get_manager(hashv);
+			if (manager == nullptr || *manager == nullptr)
 				return nullptr;
-			return &manager->value();
+			return &((**manager).value());
 		}
 
 		// 根据hashv添加值，返回值的指针
@@ -222,76 +211,93 @@ namespace ayr
 		template<typename V>
 		Value_t* set_value(V&& value, hash_t hashv)
 		{
-			Manager_t value_manager{ std::forward<V>(value), hashv, 0 };
-			Value_t* ret = nullptr;
+			Manager_t* new_manager = ayr_make<Manager_t>(std::forward<V>(value), hashv, 0);
+
+			return set_value(new_manager);
+		}
+
+		Value_t* set_value(Manager_t* new_manager)
+		{
 			c_size capacity_ = capacity();
-			for (c_size index = super::hash2index(hashv), i = 0; i < capacity_; ++i)
+			for (c_size index = super::hash2index(new_manager->hashv()), i = 0; i < capacity_; ++i)
 			{
-				Manager_t& manager = robin_managers_.at((index + i) % capacity_);
-				if (!manager.is_managed())
+				Manager_t*& manager = robin_managers_.at((index + i) % capacity_);
+				if (manager == nullptr)
 				{
-					manager = std::move(value_manager);
-					ret = &manager.value();
-					break;
+					manager = new_manager;
+					return &manager->value();
 				}
-				else if (value_manager.move_more_than(manager))
+				else if (new_manager->move_more_than(*manager))
 				{
-					value_manager.swap(manager);
+					std::swap(manager, new_manager);
 				}
-				value_manager.add_move_dist();
+				new_manager->add_move_dist();
 			}
 
-			if (ret == nullptr)
-				RuntimeError("RobinHashBucket set_value failed, bucekt is full");
-
-			return ret;
+			RuntimeError("RobinHashBucket set_value failed, bucekt is full");
+			return nullptr;
 		}
 
 		void pop(hash_t hashv)
 		{
 			c_size capacity_ = capacity();
-			Manager_t* manager = try_get_manager(hashv);
-			if (manager == nullptr || !manager->is_managed())
+			Manager_t** manager = try_get_manager(hashv);
+			if (manager == nullptr || *manager == nullptr)
 				return;
 
+			ayr_desloc(*manager, 1);
+			*manager = nullptr;
+
 			c_size cur_idx = std::distance(robin_managers_.data(), manager);
+
 			while (true)
 			{
 				c_size next_idx = (cur_idx + 1) % capacity_;
-				Manager_t& cur_manager = robin_managers_.at(cur_idx);
-				Manager_t& next_manager = robin_managers_.at(next_idx);
-				if (next_manager.is_managed() && next_manager.move_more_than(0))
+				Manager_t*& cur_manager = robin_managers_.at(cur_idx);
+				Manager_t*& next_manager = robin_managers_.at(next_idx);
+
+				if (next_manager != nullptr && next_manager->move_more_than(0))
 				{
-					next_manager.add_move_dist(-1);
-					cur_manager = std::move(next_manager);
+					next_manager->add_move_dist(-1);
+					cur_manager = next_manager;
+					next_manager = nullptr;
 				}
 				else
-				{
-					cur_manager.unmanage();
 					break;
-				}
+
 				cur_idx = next_idx;
 			}
 		}
 
 		void expand(c_size new_capacity) noexcept
 		{
-			Array<Manager_t> temp_managers(std::move(robin_managers_));
+			Array<Manager_t*> temp_managers(std::move(robin_managers_));
 			robin_managers_.resize(new_capacity);
-			for (Manager_t& manager : temp_managers)
-				if (manager.is_managed())
-					set_value(manager.value(), manager.hashv());
+
+			for (Manager_t* manager : temp_managers)
+				if (manager != nullptr)
+				{
+					manager->reset_move_dist();
+					set_value(manager);
+				}
+
 		}
 
-		void clear() { robin_managers_.resize(0); }
+		void clear()
+		{
+			for (Manager_t* manager : robin_managers_)
+				if (manager != nullptr)
+					ayr_desloc(manager, 1);
+			robin_managers_.resize(0);
+		}
 
 		template<bool IsConst>
 		class RobinHashBucketIterator :
-			public IteratorInfo<RobinHashBucketIterator<IsConst>, add_const_t<IsConst, Array<Manager_t>>, std::forward_iterator_tag, add_const_t<IsConst, Value_t>>
+			public IteratorInfo<RobinHashBucketIterator<IsConst>, add_const_t<IsConst, Array<Manager_t*>>, std::forward_iterator_tag, add_const_t<IsConst, Value_t>>
 		{
 			using self = RobinHashBucketIterator;
 
-			using ItInfo = IteratorInfo<self, add_const_t<IsConst, Array<Manager_t>>, std::forward_iterator_tag, add_const_t<IsConst, Value_t>>;
+			using ItInfo = IteratorInfo<self, add_const_t<IsConst, Array<Manager_t*>>, std::forward_iterator_tag, add_const_t<IsConst, Value_t>>;
 		public:
 			RobinHashBucketIterator() : bucket_ptr_(nullptr), index_(0) {}
 
@@ -308,14 +314,14 @@ namespace ayr
 				return *this;
 			}
 
-			ItInfo::reference operator*() const { return bucket_ptr_->at(index_).value(); }
+			ItInfo::reference operator*() const { return bucket_ptr_->at(index_)->value(); }
 
-			ItInfo::pointer operator->() const { return &bucket_ptr_->at(index_).value(); }
+			ItInfo::pointer operator->() const { return &bucket_ptr_->at(index_)->value(); }
 
 			self& operator++()
 			{
 				++index_;
-				while (index_ < bucket_ptr_->size() && !bucket_ptr_->at(index_).is_managed()) ++index_;
+				while (index_ < bucket_ptr_->size() && bucket_ptr_->at(index_) == nullptr) ++index_;
 				return *this;
 			}
 
@@ -343,7 +349,7 @@ namespace ayr
 		Iterator begin()
 		{
 			for (c_size i = 0; i < capacity(); ++i)
-				if (robin_managers_.at(i).is_managed())
+				if (robin_managers_.at(i) != nullptr)
 					return Iterator(&this->robin_managers_, i);
 			return end();
 		}
@@ -351,7 +357,7 @@ namespace ayr
 		ConstIterator begin() const
 		{
 			for (c_size i = 0; i < capacity(); ++i)
-				if (robin_managers_.at(i).is_managed())
+				if (robin_managers_.at(i) != nullptr)
 					return ConstIterator(&this->robin_managers_, i);
 			return end();
 		}
@@ -360,7 +366,7 @@ namespace ayr
 
 		ConstIterator end() const { return ConstIterator(&this->robin_managers_, capacity()); }
 	private:
-		Array<Manager_t> robin_managers_;
+		Array<Manager_t*> robin_managers_;
 	};
 }
 #endif
