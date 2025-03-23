894366f6160b33c8f6fa23461f5f48c35739c236
aurshine <2901658788@qq.com>
2025-03-23 17:17:38 +0800

    优化Eventloop的声明定义方式

--- include/ayr/net/EventLoop.hpp
+++ include/ayr/net/EventLoop.hpp
@@ -6,42 +6,53 @@
 namespace ayr
 {
 #if defined(AYR_LINUX)
-	UltraEventLoop::UltraEventLoop() : epoll_(), quit(false) {}
-
-	UltraEventLoop::~UltraEventLoop() 
+	class UltraEventLoop : public Object<UltraEventLoop>
 	{
-		for (auto&& [fd, ep_ev] : epoll_.epoll_events_.items())
-			ayr_desloc(static_cast<Channel*>(ep_ev.data.ptr), 1);
-	}
+		using self = UltraEventLoop;
 
-	void UltraEventLoop::add_channel(Channel* channel) 
-	{ 
-		epoll_.set(channel->fd(), channel, channel->events()); 
-	}
+		using super = Object<UltraEventLoop>;
 
-	void UltraEventLoop::remove_channel(Channel* channel) 
-	{ 
-		epoll_.del(channel->fd()); 
-		ayr_desloc(channel, 1);
-	}
+		Epoll epoll_;
 
-	void UltraEventLoop::stop() { quit = true; }
+		bool quit;
+	public:
+		UltraEventLoop(): epoll_(), quit(false) {}
 
-	// 运行一次事件循环，返回处理的事件数量
-	// 返回为0表示超时，返回为-1表示无法运行
-	c_size UltraEventLoop::run_once(int timeout_ms)
-	{
-		if (quit) return -1;
-		Array<epoll_event> ep_evs = epoll_.wait(timeout_ms);
-		for (auto& ep_ev : ep_evs)
+		~UltraEventLoop() 
 		{
-			Channel* channel = static_cast<Channel*>(ep_ev.data.ptr);
-			channel->set_revents(ep_ev.events);
-			channel->handle();
+			for (auto&& [fd, ep_ev] : epoll_.epoll_events_.items())
+				ayr_desloc(static_cast<Channel*>(ep_ev.data.ptr));
 		}
-			
-		return ep_evs.size();
-	}
+
+		void add_channel(Channel* channel) 
+		{ 
+			epoll_.set(channel->fd(), channel, channel->events()); 
+		}
+
+		void remove_channel(Channel* channel) 
+		{ 
+			epoll_.del(channel->fd()); 
+			ayr_desloc(channel);
+		}
+
+		void stop() { quit = true; }
+
+		// 运行一次事件循环，返回处理的事件数量
+		// 返回为0表示超时，返回为-1表示无法运行
+		c_size run_once(int timeout_ms)
+		{
+			if (quit) return -1;
+			Array<epoll_event> ep_evs = epoll_.wait(timeout_ms);
+			for (auto& ep_ev : ep_evs)
+			{
+				Channel* channel = static_cast<Channel*>(ep_ev.data.ptr);
+				channel->set_revents(ep_ev.events);
+				channel->handle();
+			}
+				
+			return ep_evs.size();
+		}	
+	};
 #endif // AYR_LINUX
 }
 #endif // AYR_NET_EVENTLOOP_HPP
