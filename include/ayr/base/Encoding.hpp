d69c0885482bb89b5fa9c3e9cbe9129a688755f7
aurshine <2901658788@qq.com>
2025-04-11 22:47:48 +0800

    添加to_int寒素

--- include/ayr/base/Encoding.hpp
+++ include/ayr/base/Encoding.hpp
@@ -91,7 +91,38 @@ namespace ayr
 
 		CString from_int(int code) const override
 		{
-			NotImplementedError("UTF8Encoding::from_int is not suppoerted yet");
+			if (code < 0 || code > 0x10FFFF)
+				EncodingError("Invalid AChar");
+
+			char buf[4];
+			if (code <= 0x7F)
+			{
+				buf[0] = code;
+				return CString(buf, 1);
+			}
+			else if (code <= 0x7FF)
+			{
+				buf[0] = 0xC0 | (code >> 6);
+				buf[1] = 0x80 | (code & 0x3F);
+				return CString(buf, 2);
+			}
+			else if (code <= 0xFFFF)
+			{
+				buf[0] = 0xE0 | (code >> 12);
+				buf[1] = 0x80 | ((code >> 6) & 0x3F);
+				buf[2] = 0x80 | (code & 0x3F);
+				return CString(buf, 3);
+			}
+			else if (code <= 0x10FFFF)
+			{
+				buf[0] = 0xF0 | (code >> 18);
+				buf[1] = 0x80 | ((code >> 12) & 0x3F);
+				buf[2] = 0x80 | ((code >> 6) & 0x3F);
+				buf[3] = 0x80 | (code & 0x3F);
+				return CString(buf, 4);
+			}
+
+			EncodingError("Invalid AChar");
 			return CString();
 		}
 	};
@@ -141,7 +172,27 @@ namespace ayr
 
 		CString from_int(int code) const override
 		{
-			NotImplementedError("UTF16Encoding::from_int is not suppoerted yet");
+			if (code < 0 || code > 0x10FFFF)
+				EncodingError("Invalid AChar");
+
+			char buf[4];
+			if (code <= 0xFFFF)
+			{
+				buf[0] = code >> 8;
+				buf[1] = code & 0xFF;
+				return CString(buf, 2);
+			}
+			else if (code <= 0x10FFFF)
+			{
+				code -= 0x10000;
+				buf[0] = 0xD8 | (code >> 18);
+				buf[1] = 0xDC | ((code >> 10) & 0x3F);
+				buf[2] = 0xDC | (code & 0x3F);
+				buf[3] = 0xDC | ((code >> 22) & 0x3F);
+				return CString(buf, 4);
+			}
+
+			EncodingError("Invalid AChar");
 			return CString();
 		}
 	};
@@ -167,8 +218,12 @@ namespace ayr
 
 		CString from_int(int code) const override
 		{
-			NotImplementedError("UTF32Encoding::from_int is not suppoerted yet");
-			return CString();
+			char buf[4];
+			buf[0] = code >> 24;
+			buf[1] = (code >> 16) & 0xFF;
+			buf[2] = (code >> 8) & 0xFF;
+			buf[3] = code & 0xFF;
+			return CString(buf, 4);
 		}
 	};
 
