d4ae2225ec28d9cbfdca16e44d061c9104f9048e
aurshine <2901658788@qq.com>
2025-05-05 14:41:58 +0800

    优化ViewOf实现

--- include/ayr/base/View.hpp
+++ include/ayr/base/View.hpp
@@ -61,43 +61,73 @@ namespace ayr
 	template<typename T>
 	class ViewOF
 	{
+		static_assert(std::is_object_v<T> || std::is_function_v<T>, "T must be an object or function type");
+
 		View view_;
 
-		using ConstValue_t = std::add_const_t<T>;
+		using Value_t = std::remove_const_t<T>;
+
+		using self = ViewOF<T>;
+
+		friend ViewOF<const Value_t>;
+
+		friend ViewOF<Value_t>;
 	public:
-		ViewOF(ConstValue_t& obj): view_(obj) {}
+		ViewOF(const Value_t& obj) : view_(obj) {}
 
-		ViewOF(const ViewOF& other): view_(other.view_) {}
+		ViewOF(Value_t&& obj) = delete;
 
-		ViewOF& operator=(ConstValue_t& obj)
+		ViewOF(const ViewOF<Value_t>& other) : view_(other.view_) {}
+
+		ViewOF(const ViewOF<const Value_t>& other) : view_(other.view_) {}
+
+		self& operator=(const Value_t& obj)
 		{
 			view_ = obj;
 			return *this;
 		}
 
-		ViewOF& operator=(const ViewOF& other)
+		self& operator=(Value_t&& obj) = delete;
+
+		self& operator=(const ViewOF<Value_t>& other)
 		{
 			view_ = other.view_;
 			return *this;
 		}
 
-		ConstValue_t& get() const { return view_.get<ConstValue_t>(); }
+		self& operator=(const ViewOF<const Value_t>& other)
+		{
+			view_ = other.view_;
+			return *this;
+		}
 
-		operator ConstValue_t& () const { return get(); }
+		const Value_t& get() const { return view_.get<const Value_t>(); }
+
+		const Value_t* operator->() const { return static_cast<const Value_t*>(view_.data()); }
+
+		template<typename... Args>
+		auto operator()(Args&&... args) const
+		{
+			return std::invoke(get(), std::forward<Args>(args)...);
+		}
+
+		operator const Value_t& () const { return get(); }
 
 		CString __str__() const { return cstr(get()); }
 
 		void __repr__(Buffer& buffer) const { buffer << get(); }
 
-		bool __equals__(const ConstValue_t& other) const { return view_.__equals__(other); }
+		bool __equals__(const Value_t& other) const { return view_.__equals__(other); }
 
-		void __swap__(ConstValue_t& other) { swap(view_, other); }
+		void __swap__(ViewOF<Value_t>& other) { swap(view_, other.view_); }
+
+		void __swap__(ViewOF<const Value_t>& other) { swap(view_, other.view_); }
 	};
 
 	template<typename T>
-	def view_of(T& obj) { return ViewOF<std::remove_const_t<T>>(obj); }
+	def view_of(T& obj) { return ViewOF<T>(obj); }
 
 	template<typename T>
-	def view_of(const ViewOF<T>& obj) { return ViewOF<T>(obj); }
+	def view_of(ViewOF<T> obj) { return obj; }
 }
 #endif
