ddc4cb608241d3b2f779467aaacb5fd40357ba62
aurshine <2901658788@qq.com>
2025-07-08 14:29:35 +0800

    根据CString的重构，修改部分函数的实现

--- include/ayr/fs/Path.hpp
+++ include/ayr/fs/Path.hpp
@@ -1,17 +1,21 @@
 #ifndef AYR_FS_PATH_HPP
 #define AYR_FS_PATH_HPP
 
-#include "oslib.h"
+#include <queue>
 
-#if defined(AYR_WIN)
+#include "oslib.h"
+#include "../coro/Generator.hpp"
 #include "../DynArray.hpp"
 
-#include <queue>
 namespace ayr
 {
 	namespace fs
 	{
+#ifdef AYR_WIN
 		constexpr char PATH_SEP = '\\';
+#elif
+		constexpr char PATH_SEP = '/';
+#endif
 
 		bool is_sep(char c) { return c == '\\' || c == '/'; }
 
@@ -22,45 +26,47 @@ namespace ayr
 		{
 			c_size len1 = path1.size();
 			c_size len2 = path2.size();
-			if (is_sep(path1[len1 - 1]))
+			while (is_sep(path1[len1 - 1]))
 				--len1;
 
-			auto s = path2.data();
-			if (is_sep(path2[0]))
+			const char* s = path2.data();
+			while (is_sep(*s))
 				++s, --len2;
 
-			return CString::cjoin(arr(path1, cstr(PATH_SEP), s));
+			return CString::cjoin(arr(vstr(path1.data(), len1), cstr(PATH_SEP), vstr(s, len2)));
 		}
 
 		void find_first_file_cb(const CString& path, const CString& pattern, const std::function<void(const WIN32_FIND_DATAA& find_data)>& callback)
 		{
 			WIN32_FIND_DATAA find_data;
 
-			HANDLE handle = FindFirstFileA(join(path, pattern).data(), &find_data);
+			HANDLE handle = FindFirstFileA(join(path, pattern).c_str().c_str(), &find_data);
+
+			ExTask task([&handle] { FindClose(handle); });
 
 			if (handle == INVALID_HANDLE_VALUE)
 				system_error(path);
+			
 			do
 			{
-				if (strcmp(find_data.cFileName, ".") == 0 || strcmp(find_data.cFileName, "..") == 0)
+				if (std::strcmp(find_data.cFileName, ".") == 0 || 
+					std::strcmp(find_data.cFileName, "..") == 0)
 					continue;
 
 				callback(find_data);
 			} while (FindNextFileA(handle, &find_data));
-
-			FindClose(handle);
 		}
 
 		// 是否存在路径
 		def exists(const CString& path)
 		{
-			return GetFileAttributesA(path.data()) != INVALID_FILE_ATTRIBUTES;
+			return GetFileAttributesA(path.c_str().c_str()) != INVALID_FILE_ATTRIBUTES;
 		}
 
 		// 是否是文件
 		def isfile(const CString& path)
 		{
-			DWORD attributes = GetFileAttributesA(path.data());
+			DWORD attributes = GetFileAttributesA(path.c_str().c_str());
 
 			return (attributes != INVALID_FILE_ATTRIBUTES) && (attributes & FILE_ATTRIBUTE_DIRECTORY) != 0;
 		}
@@ -68,42 +74,44 @@ namespace ayr
 		// 是否是文件夹
 		def isdir(const CString& path)
 		{
-			return (GetFileAttributesA(path.data()) & FILE_ATTRIBUTE_DIRECTORY) != 0;
+			return (GetFileAttributesA(path.c_str().c_str()) & FILE_ATTRIBUTE_DIRECTORY) != 0;
 		}
 
 		// 是否是绝对路径
 		def isabs(const CString& path)
 		{
-			return (path[0] && path[1] == ':' && is_sep(path[2])) || (is_sep(path[0]) && is_sep(path[1]));
+			return (path.size() > 3 && path[1] == ':' && is_sep(path[2])) || 
+				   (path.size() > 2 && is_sep(path[0]) && is_sep(path[1]));
 		}
 
 		// 获得绝对路径
 		def abspath(const CString& path)
 		{
-			char* full_path = ayr_alloc<char>(MAX_PATH);
-			DWORD len = GetFullPathNameA(path.data(), MAX_PATH, full_path, nullptr);
+			char full_path[MAX_PATH];
+			DWORD len = GetFullPathNameA(path.c_str().c_str(), MAX_PATH, full_path, nullptr);
 
 			if (len == 0 || len > MAX_PATH)
 				SystemError(std::format("abspath failed for path: {}", path));
 
-			return ostr(full_path, len);
+			return dstr(full_path, len);
 		}
 
+		// 获得当前路径
 		def getcwd()
 		{
-			char* current_path = ayr_alloc<char>(MAX_PATH);
+			char current_path[MAX_PATH];
 			DWORD len = GetCurrentDirectoryA(MAX_PATH, current_path);
 
 			if (len == 0 || len > MAX_PATH)
 				SystemError("getcwd failed");
 
-			return ostr(current_path, len);
+			return dstr(current_path, len);
 		}
 
 		// 用当前路径创建一个文件夹
 		def mkdir(const CString& path, bool exist_ok = false)
 		{
-			BOOL state = CreateDirectoryA(path.data(), nullptr);
+			BOOL state = CreateDirectoryA(path.c_str().c_str(), nullptr);
 
 			switch (state)
 			{
@@ -122,16 +130,16 @@ namespace ayr
 		{
 			DynArray<CString> results;
 			find_first_file_cb(path, "*", [&results](const WIN32_FIND_DATAA& find_data) { results.append(find_data.cFileName); });
-			return results.to_array();
+			return results.move_array();
 		}
 
 		// 删除当前路径的文件或文件夹
 		def remove(const CString& path) -> void
 		{
-			DWORD attr = GetFileAttributesA(path.data());
+			DWORD attr = GetFileAttributesA(path.c_str().c_str());
 
 			if (attr & FILE_ATTRIBUTE_READONLY)
-				SetFileAttributesA(path.data(), attr & ~FILE_ATTRIBUTE_READONLY);
+				SetFileAttributesA(path.c_str().c_str(), attr & ~FILE_ATTRIBUTE_READONLY);
 
 			if (attr != INVALID_FILE_ATTRIBUTES)
 			{
@@ -139,15 +147,15 @@ namespace ayr
 				{
 					for (auto& sub_path : listdir(path))
 						remove(join(path, sub_path));
-					RemoveDirectoryA(path.data());
+					RemoveDirectoryA(path.c_str().c_str());
 				}
 				else
-					DeleteFileA(path.data());
+					DeleteFileA(path.c_str().c_str());
 			}
 		}
 
 		// 遍历当前路径下的所有文件和文件夹
-		def walk(const CString& path)
+		def walk(const CString& path) -> coro::Generator<std::tuple<CString, Array<CString>, Array<CString>>>
 		{
 			DynArray<std::tuple<CString, Array<CString>, Array<CString>>> results;
 			std::queue<CString> root_dirs;
@@ -155,19 +163,18 @@ namespace ayr
 
 			while (!root_dirs.empty())
 			{
-				CString root = root_dirs.front();
+				CString root = std::move(root_dirs.front());
 				root_dirs.pop();
 
 				DynArray<CString> dirs, files;
 				find_first_file_cb(root, "*", [&dirs, &files](const WIN32_FIND_DATAA& find_data) {
 					if ((find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0)
-						dirs.append(find_data.cFileName);
+						dirs.append(dstr(find_data.cFileName));
 					else
-						files.append(find_data.cFileName);
+						files.append(dstr(find_data.cFileName));
 					});
-				results.append(std::make_tuple(std::move(root), dirs.to_array(), files.to_array()));
+				co_yield std::make_tuple(std::move(root), dirs.move_array(), files.move_array());
 			}
-			return results;
 		}
 
 		// 获得当前路径的文件或文件夹大小，单位为字节
@@ -177,7 +184,7 @@ namespace ayr
 			if (isfile(path))
 			{
 				WIN32_FILE_ATTRIBUTE_DATA attr;
-				if (!GetFileAttributesExA(path.data(), GetFileExInfoStandard, &attr))
+				if (!GetFileAttributesExA(path.c_str().c_str(), GetFileExInfoStandard, &attr))
 					system_error(path);
 				size_ = (static_cast<uint64_t>(attr.nFileSizeHigh) << 32) | attr.nFileSizeLow;
 			}
@@ -192,28 +199,35 @@ namespace ayr
 		// 获得路径的基本名
 		def basename(const CString& path)
 		{
-			c_size last_sep_pos = -1, pos = 0;
-			while (path[pos])
-			{
-				if (is_sep(path[pos])) last_sep_pos = pos;
-				++pos;
-			}
+			c_size last_sep_pos = -1;
+			for (c_size i = path.size() - 1; i >= 0; -- i)
+				if (is_sep(path[i]))
+				{
+					last_sep_pos = i;
+					break;
+				}
 
-			return dstr(path.data() + last_sep_pos + 1, pos - last_sep_pos - 1);
+			return path.slice(last_sep_pos + 1);
 		}
 
 		// 获得路径的目录
-		def dirname(const CString& path)
+		def dirname(const CString& path) -> CString
 		{
-			c_size last_sep_pos = 0, pos = 0;
-			while (is_sep(path[pos])) ++pos, ++last_sep_pos;
-			while (path[pos])
-			{
-				if (is_sep(path[pos])) last_sep_pos = pos;
-				++pos;
-			}
+			c_size last_sep_pos = -1;
+			for (c_size i = path.size() - 1; i >= 0; -- i)
+				if (is_sep(path[i]))
+				{
+					last_sep_pos = i;
+					break;
+				}
+					
+			
+			if (last_sep_pos == -1)
+				return "";
+			else if (last_sep_pos == 0)
+				return cstr(PATH_SEP);
 
-			return dstr(path.data(), last_sep_pos);
+			return path.slice(0, last_sep_pos);
 		}
 
 		// 获得路径的目录和文件名
@@ -226,21 +240,21 @@ namespace ayr
 		def splitext(const CString& path)
 		{
 			c_size last_dot_pos = -1, pos = 0;
-			while (path[pos] == '.') ++pos;
-			while (path[pos])
-			{
-				if (path[pos] == '.') last_dot_pos = pos;
-				++pos;
-			}
+			for (c_size i = path.size() - 1; i >= 0; -- i)
+				if (path[i] == '.')
+				{
+					last_dot_pos = i;
+					break;
+				}
+					
 
-			if (last_dot_pos == -1) last_dot_pos = pos;
+			if (last_dot_pos == -1) last_dot_pos = path.size();
+
 			return std::make_pair(
-				dstr(path.data(), last_dot_pos),
-				dstr(path.data() + last_dot_pos, pos - last_dot_pos)
+				path.slice(0, last_dot_pos),
+				path.slice(last_dot_pos)
 			);
 		}
 	}
 }
-#elif defined(AYR_LINUX)
-#endif 
 #endif // AYR_FS_PATH_HPP
