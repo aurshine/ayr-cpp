22446c867d0c3665bbc2b0749b74a7a8cf16a93f
aurshine <2901658788@qq.com>
2025-09-20 16:21:34 +0800

    所有错误用AyrError类抛出，不建议捕获

--- include/ayr/base/raise_error.hpp
+++ include/ayr/base/raise_error.hpp
@@ -5,58 +5,80 @@
 
 namespace ayr
 {
-	template<Printable T>
-	inline void warn_assert(bool condition, const T& msg, const ::std::source_location& loc = ::std::source_location::current())
+	// 错误类，抛出后不应该被捕获
+	class AyrError : public Object<AyrError>
 	{
-		if (!condition)
+		using self = AyrError;
+
+		using super = Object<self>;
+
+		CString err_msg_;
+	public:
+		AyrError(const CString& msg) : err_msg_(msg) {}
+
+		AyrError(CString&& msg) : err_msg_(std::move(msg)) {}
+
+		AyrError(const self& other) : err_msg_(other.err_msg_) {}
+
+		AyrError(self&& other) : err_msg_(std::move(other.err_msg_)) {}
+
+		self& operator=(const self& other)
 		{
-			ayr_warner(
-				std::format("file: {}\nline, column: {}, {}\nfunction_name: {}\nerror:",
-					loc.file_name(),
-					loc.line(),
-					loc.column(),
-					loc.function_name()),
-				msg
-			);
+			if (this == &other) return *this;
+			err_msg_ = other.err_msg_;
+			return *this;
 		}
-	}
 
-	template<Printable T>
-	inline void error_exec(const T& msg, const ::std::source_location& loc = ::std::source_location::current())
+		self& operator=(self&& other) noexcept
+		{
+			if (this == &other) return *this;
+			err_msg_ = std::move(other.err_msg_);
+			return *this;
+		}
+
+		CString what() const noexcept { return err_msg_; }
+	};
+
+	/*
+	* 打印错误信息
+	*
+	* @param err_name 错误名称
+	*
+	* @param msg 错误信息
+	*/
+	template<typename T>
+	def pems(const CString& err_name, T&& msg, const ::std::source_location& loc = ::std::source_location::current()) -> CString
 	{
-		ayr_error(
-			std::format("file: {}\nline, column: {}, {}\nfunction_name: {}\n",
-				loc.file_name(),
-				loc.line(),
-				loc.column(),
-				loc.function_name()),
-			msg
-		);
-
-		throw std::runtime_error("");
+		Buffer buffer;
+		buffer << "File: " << loc.file_name() << "\n";
+		buffer << "Line, Column: " << loc.line() << ", " << loc.column() << "\n";
+		buffer << "FunctionName: " << loc.function_name() << "\n";
+		buffer << err_name << ": " << cstr(msg) << "\n";
+		ayr_error(vstr(buffer.peek(), buffer.readable_size()));
+		return from_buffer(std::move(buffer));
 	}
 
+	// 抛出一个错误，并打印错误信息，错误不应该被捕获
+#define RAISE(E_NAME, E_MSG) throw ayr::AyrError(pems(E_NAME, E_MSG))
 
-#define Error(errorname, msg) error_exec(std::format("{}: {}", errorname, msg))
+#define KeyError(E_MSG) RAISE("KeyError", E_MSG)
 
-#define KeyError(msg) Error("KeyError", msg)
+#define ValueError(E_MSG) RAISE("ValueError", E_MSG)
 
-#define ValueError(msg) Error("ValueError", msg)
+#define TypeError(E_MSG) RAISE("TypeError", E_MSG)
 
-#define TypeError(msg) Error("TypeError", msg)
+#define RuntimeError(E_MSG) RAISE("RuntimeError", E_MSG)
 
-#define RuntimeError(msg) Error("RuntimeError", msg)
+#define NotImplementedError(E_MSG) RAISE("NotImplementedError", E_MSG)
 
-#define NotImplementedError(msg) Error("NotImplementedError", msg)
+#define NullPointerError(E_MSG) RAISE("NullPointerError", E_MSG)
 
-#define NullPointerError(msg) Error("NullPointerError", msg)
+#define FileNotFoundError(E_MSG) RAISE("FileNotFoundError", E_MSG)
 
-#define FileNotFoundError(msg) Error("FileNotFoundError", msg)
+#define PermissionError(E_MSG) RAISE("PermissionError", E_MSG)
 
-#define PermissionError(msg) Error("PermissionError", msg)
+#define EncodingError(E_MSG) RAISE("EncodingError", E_MSG)
 
-#define EncodingError(msg) Error("EncodingError", msg)
-
-#define SystemError(msg) Error("SystemError", msg)
+#define SystemError(E_MSG) RAISE("SystemError", E_MSG)
 }
 #endif // AYR_BASE_RAISE_ERROR_HPP
