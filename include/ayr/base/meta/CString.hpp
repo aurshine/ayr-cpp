88988db42600052b58dd44f851ecdecdb81fe0d5
aurshine <2901658788@qq.com>
2025-10-18 00:23:34 +0800

    [FIX] 优化CString的编译期行为

--- include/ayr/base/meta/CString.hpp
+++ include/ayr/base/meta/CString.hpp
@@ -22,10 +22,18 @@ namespace ayr
 	template<size_t N>
 	constexpr c_size strlen(const char(&str)[N]) { return N - 1; }
 
-	constexpr void memcpy(void* dst, const void* src, c_size size)
+	constexpr void strcpy(char* dst, const char* src, c_size size)
 	{
 		for (c_size i = 0; i < size; ++i)
-			reinterpret_cast<char*>(dst)[i] = reinterpret_cast<const char*>(src)[i];
+			dst[i] = src[i];
+	}
+
+	constexpr int strcmp(const char* str1, const char* str2, c_size min_len)
+	{
+		for (c_size i = 0; i < min_len; ++i)
+			if (str1[i] != str2[i])
+				return str1[i] - str2[i];
+		return 0;
 	}
 
 	/**
@@ -43,7 +51,7 @@ namespace ayr
 	{
 		using self = CString;
 
-		// 最高位字节为1，x & OWNER_MASK == 1 表示内存属于该对象
+		// 最高位字节为1，x & OWNER_MASK == 1 表示内存属于该对象，内存的生命周期随对象控制
 		static constexpr c_size OWNER_MASK = 1ll << 63;
 
 		// 最高位字节为1，x & SSO_MASK == 1 表示是sso优化
@@ -66,56 +74,62 @@ namespace ayr
 		};
 	public:
 		// 空字符串，不占有内存，不使用sso优化
-		constexpr CString() : long_str(""), owner_sso_length_flag(0) {}
+		constexpr CString() : short_str(), owner_sso_length_flag(SSO_MASK) {}
 
 		// 浅拷贝，根据owner参数决定是否占有内存，不使用sso优化
 		constexpr CString(const char* str_, c_size len = -1, bool owner = false) : long_str(str_)
 		{
-			if (len == -1) len = strlen(str_);
+			if (len == -1) len = ayr::strlen(str_);
 			if (owner)
 				owner_sso_length_flag = len | OWNER_MASK;
 			else
 				owner_sso_length_flag = len;
 		}
 
-		// 浅拷贝，根据owner参数决定是否占有内存，不使用sso优化
-		template<size_t N>
-		constexpr CString(const char(&str_)[N], bool owner = false) : CString(str_, N - 1, owner) {}
-
 		// 浅拷贝，不占有内存，不使用sso优化
 		constexpr CString(const self& other) : CString(other.data(), other.size(), false) {}
 
+		// 如果other是sso优化的，则直接赋值，否则代替内存占用
 		constexpr CString(self&& other) noexcept
 		{
 			if (other.sso())
-				memcpy(short_str, other.short_str, SSO_SIZE);
+				ayr::strcpy(short_str, other.short_str, SSO_SIZE);
 			else
 				long_str = other.long_str;
 			owner_sso_length_flag = other.owner_sso_length_flag;
 			other.owner_sso_length_flag = 0;
 		}
 
-		~CString()
+		constexpr ~CString()
 		{
+			// long_str的内存是char*分配的，需要转换为char*再释放
 			if (owner())
 				ayr_delloc(const_cast<char*>(long_str));
 			owner_sso_length_flag = 0;
 		}
 
-		self& operator=(const self& other)
+		constexpr self& operator=(const self& other)
 		{
 			if (this == &other) return *this;
-			ayr_destroy(this);
+			this->~CString();
 
-			return *ayr_construct(this, other);
+			this->long_str = other.data();
+			this->owner_sso_length_flag = other.size();
+			return *this;
 		}
 
-		self& operator=(self&& other) noexcept
+		constexpr self& operator=(self&& other) noexcept
 		{
 			if (this == &other) return *this;
-			ayr_destroy(this);
+			this->~CString();
 
-			return *ayr_construct(this, std::move(other));
+			if (other.sso())
+				ayr::strcpy(short_str, other.short_str, SSO_SIZE);
+			else
+				long_str = other.long_str;
+			owner_sso_length_flag = other.owner_sso_length_flag;
+			other.owner_sso_length_flag = 0;
+			return *this;
 		}
 
 		// 判断是否拥有内存
@@ -143,39 +157,54 @@ namespace ayr
 		std::string c_str() const { return std::string(data(), size()); }
 
 		// 拷贝一个新的 CString 对象，并拥有内存
-		self clone() const
+		constexpr self clone() const
 		{
-			return sso_assign(size(), [&](char*& ptr) {
-				std::memcpy(ptr, this->data(), this->size());
-				});
+			self new_str;
+			c_size m_size = size();
+			if (m_size > SSO_SIZE)
+			{
+				char* ptr = ayr_alloc<char>(m_size);
+				ayr::strcpy(ptr, this->data(), m_size);
+				new_str.long_str = ptr;
+				// 无小内存优化
+				new_str.owner_sso_length_flag = m_size | OWNER_MASK;
+			}
+			else
+			{
+				// 小内存优化
+				ayr::strcpy(new_str.short_str, this->data(), m_size);
+				new_str.owner_sso_length_flag = m_size | SSO_MASK;
+			}
+
+			return new_str;
 		}
 
 		// 字符串切片，[start, end)，浅拷贝
-		self vslice(c_size start, c_size end) const { return self(data() + start, end - start, false); }
+		constexpr self vslice(c_size start, c_size end) const { return self(data() + start, end - start, false); }
 
 		// 字符串切片，[start, size())，浅拷贝
-		self vslice(c_size start) const { return vslice(start, size()); }
+		constexpr self vslice(c_size start) const { return vslice(start, size()); }
 
 		// 字符串切片，[start, end)，深拷贝
-		self slice(c_size start, c_size end) const { return vslice(start, end).clone(); }
+		constexpr self slice(c_size start, c_size end) const { return vslice(start, end).clone(); }
 
 		// 字符串切片，[start, size())，深拷贝
-		self slice(c_size start) const { return slice(start, size()); }
+		constexpr self slice(c_size start) const { return vslice(start, size()).clone(); }
 
 		// 判断是否以prefix开头
-		bool startswith(const self& preifx) const
+		constexpr bool startswith(const self& preifx) const
 		{
 			c_size m_size = size(), p_size = preifx.size();
 			if (p_size > m_size) return false;
-			return std::memcmp(data(), preifx.data(), p_size) == 0;
+			return ayr::strcmp(data(), preifx.data(), p_size) == 0;
 		}
 
 		// 判断是否以suffix结尾
-		bool endswith(const self& suffix) const
+		constexpr bool endswith(const self& suffix) const
 		{
 			c_size m_size = size(), s_size = suffix.size();
 			if (s_size > m_size) return false;
-			return std::memcmp(data() + m_size - s_size, suffix.data(), s_size) == 0;
+			return ayr::strcmp(data() + m_size - s_size, suffix.data(), s_size) == 0;
 		}
 
 		// 通过*this，连接可迭代对象中的字符串
@@ -196,10 +225,10 @@ namespace ayr
 					c_size o_size = it->size();
 					if (it != elems.begin())
 					{
-						std::memcpy(ptr, this->data(), s_size);
+						ayr::strcpy(ptr, this->data(), s_size);
 						ptr += s_size;
 					}
-					std::memcpy(ptr, it->data(), o_size);
+					ayr::strcpy(ptr, it->data(), o_size);
 					ptr += o_size;
 				}
 				});
@@ -216,76 +245,92 @@ namespace ayr
 				for (const self& s : elems)
 				{
 					c_size s_size = s.size();
-					std::memcpy(ptr, s.data(), s_size);
+					ayr::strcpy(ptr, s.data(), s_size);
 					ptr += s_size;
 				}
 				});
 		}
 
-		const char* begin() const { return data(); }
+		constexpr const char* begin() const { return data(); }
 
-		const char* end() const { return data() + size(); }
+		constexpr const char* end() const { return data() + size(); }
 
-		cmp_t __cmp__(const self& other) const
+		constexpr cmp_t __cmp__(const self& other) const
 		{
 			c_size min_size = std::min(size(), other.size());
-			int cmp = std::memcmp(data(), other.data(), min_size);
+			int cmp = ayr::strcmp(data(), other.data(), min_size);
 			if (cmp) return cmp;
 			return size() - other.size();
 		}
 
-		bool __equals__(const self& other) const
+		constexpr bool __equals__(const self& other) const
 		{
 			c_size m_size = size(), o_size = other.size();
 			if (m_size != o_size) return false;
-			return std::memcmp(data(), other.data(), m_size) == 0;
+			return ayr::strcmp(data(), other.data(), m_size) == 0;
 		}
 
-		bool __equals__(const char* other) const
+		constexpr bool __equals__(const char* other) const
 		{
-			c_size m_size = size(), o_size = strlen(other);
+			c_size m_size = size(), o_size = ayr::strlen(other);
 			if (m_size != o_size) return false;
-			return std::memcmp(data(), other, m_size) == 0;
+			return ayr::strcmp(data(), other, m_size) == 0;
 		}
 
 		size_t __hash__() const { return bytes_hash(data(), size()); }
 
 		void __repr__(Buffer& buffer) const { buffer.append_bytes(data(), size()); }
 
-		self __str__() const { return this->clone(); }
+		constexpr self __str__() const { return *this; }
 
-		bool operator> (const self& other) const { return __cmp__(other) > 0; }
+		constexpr bool operator> (const self& other) const { return __cmp__(other) > 0; }
 
-		bool operator< (const self& other) const { return __cmp__(other) < 0; }
+		constexpr bool operator< (const self& other) const { return __cmp__(other) < 0; }
 
-		bool operator>= (const self& other) const { return __cmp__(other) >= 0; }
+		constexpr bool operator>= (const self& other) const { return __cmp__(other) >= 0; }
 
-		bool operator<= (const self& other) const { return __cmp__(other) <= 0; }
+		constexpr bool operator<= (const self& other) const { return __cmp__(other) <= 0; }
 
-		bool operator== (const self& other) const { return __cmp__(other) == 0; }
+		constexpr bool operator== (const self& other) const { return __cmp__(other) == 0; }
 
-		bool operator!= (const self& other) const { return __cmp__(other) != 0; }
+		constexpr bool operator!= (const self& other) const { return __cmp__(other) != 0; }
 
-		bool operator== (const char* other) const { return __equals__(other); }
+		constexpr bool operator== (const char* other) const { return __equals__(other); }
 
-		bool operator!= (const char* other) const { return __equals__(other); }
+		constexpr bool operator!= (const char* other) const { return __equals__(other); }
 
-		self operator+(const self& other)
+		constexpr self operator+(const self& other) const
 		{
-			c_size s_size = size(), o_size = other.size();
+			c_size m_size = size(), o_size = other.size();
+			self new_str;
 
-			return sso_assign(s_size + o_size, [&](char*& ptr) {
-				std::memcpy(ptr, this->data(), s_size);
-				std::memcpy(ptr + s_size, other.data(), o_size);
-				});
+			if (m_size + o_size > SSO_SIZE)
+			{
+				char* ptr = ayr_alloc<char>(m_size + o_size);
+				ayr::strcpy(ptr, this->data(), m_size);
+				ayr::strcpy(ptr + m_size, other.data(), o_size);
+				new_str.long_str = ptr;
+				// 无小内存优化
+				new_str.owner_sso_length_flag = (m_size + o_size) | OWNER_MASK;
+			}
+			else
+			{
+				// 小内存优化
+				ayr::strcpy(new_str.short_str, this->data(), m_size);
+				ayr::strcpy(new_str.short_str + m_size, other.data(), o_size);
+				new_str.owner_sso_length_flag = (m_size + o_size) | SSO_MASK;
+			}
+
+			return new_str;
 		}
 
-		self& operator+=(const self& other)
+		constexpr self& operator+=(const self& other)
 		{
-			if (sso() && size() + other.size() <= SSO_SIZE)
+			c_size m_size = size(), o_size = other.size();
+			if (sso() && m_size + o_size <= SSO_SIZE)
 			{
-				std::memcpy(short_str + size(), other.data(), other.size());
-				owner_sso_length_flag = (size() + other.size()) | SSO_MASK;
+				ayr::strcpy(short_str + m_size, other.data(), o_size);
+				owner_sso_length_flag = (m_size + o_size) | SSO_MASK;
 			}
 			else
 			{
@@ -302,17 +347,18 @@ namespace ayr
 		static self sso_assign(c_size len, std::function<void(char*&)> fn)
 		{
 			self new_str;
-			char* tmp = nullptr, * ptr = nullptr;
+			char* ptr = nullptr;
 			if (len > SSO_SIZE)
-				ptr = tmp = ayr_alloc<char>(len);
+				ptr = ayr_alloc<char>(len);
 			else
 				ptr = new_str.short_str;
 
 			fn(ptr);
 
+			// 无小内存优化
 			if (len > SSO_SIZE)
 			{
-				new_str.long_str = tmp;
+				new_str.long_str = ptr;
 				new_str.owner_sso_length_flag = len | OWNER_MASK;
 			}
 			else
@@ -325,27 +371,59 @@ namespace ayr
 
 	// owner string
 	// 浅拷贝，并拥有str内存
-	def ostr(const char* str, c_size len = -1) { return CString(str, len, true); }
-
-	def ostr(const std::string& str) { return CString(str.c_str(), str.size(), true); }
-
-	def ostr(const std::string_view& str) { return CString(str.data(), str.size(), true); }
+	constexpr def ostr(const char* str, c_size len = -1) { return CString(str, len, true); }
 
 	// view string
 	// 浅拷贝，不拥有str内存
-	def vstr(const char* str, c_size len = -1) { return CString(str, len, false); }
+	constexpr  def vstr(const char* str, c_size len = -1) { return CString(str, len, false); }
 
-	def vstr(const std::string& str) { return CString(str.c_str(), str.size(), false); }
+	// 浅拷贝，不拥有str内存
+	constexpr  def vstr(const std::string& str) { return CString(str.c_str(), str.size(), false); }
 
-	def vstr(const std::string_view& str) { return CString(str.data(), str.size(), false); }
+	// 浅拷贝，不拥有str内存
+	constexpr  def vstr(const std::string_view& str) { return CString(str.data(), str.size(), false); }
 
 	// deep string
 	// 深拷贝, 并拥有str内存
-	def dstr(const char* str, c_size len = -1) { return vstr(str, len).clone(); }
+	constexpr def dstr(const char* str, c_size len = -1) { return vstr(str, len).clone(); }
 
-	def dstr(const std::string& str) { return vstr(str.c_str(), str.size()).clone(); }
+	// 深拷贝, 并拥有str内存
+	constexpr def dstr(const std::string& str) { return vstr(str.c_str(), str.size()).clone(); }
 
-	def dstr(const std::string_view& str) { return vstr(str.data(), str.size()).clone(); }
+	// 深拷贝, 并拥有str内存
+	constexpr def dstr(const std::string_view& str) { return vstr(str.data(), str.size()).clone(); }
+
+	// nullptr 转换为 CString 对象
+	constexpr der(CString) cstr(nullptr_t) { return vstr("nullptr", 7); }
+
+	// 将bool 转换为 CString 对象
+	constexpr der(CString) cstr(bool value) { return ifelse(value, vstr("true", 4), vstr("false", 5)); }
+
+	// 将char 转换为 CString 对象
+	constexpr der(CString) cstr(char value) { return dstr(&value, 1); }
+
+	// 将const char* 转换为 CString 对象
+	constexpr der(CString) cstr(const char* value, c_size len = -1) { return dstr(value, len); }
+
+	// 将std::string 转换为 CString 对象
+	constexpr der(CString) cstr(const std::string& value) { return dstr(value); }
+
+	// 将std::string_view 转换为 CString 对象
+	constexpr der(CString) cstr(const std::string_view& value) { return dstr(value); }
+
+	// 将任意类型转换为 CString 对象
+	template<typename T>
+	der(CString) cstr(const T& value)
+	{
+		if constexpr (hasmethod(T, __str__))
+			return value.__str__();
+		else
+		{
+			Buffer buffer;
+			buffer << value;
+			return from_buffer(std::move(buffer));
+		}
+	}
 
 	/*
 	* @brief 尝试取代buffer的内存构造 CString 对象
@@ -361,42 +439,6 @@ namespace ayr
 			return res;
 		}
 		return dstr(buffer.peek(), buffer.readable_size());
-	}
-
-	// nullptr 转换为 CString 对象
-	der(CString) cstr(nullptr_t) { return vstr("nullptr", 7); }
-
-	// 将bool 转换为 CString 对象
-	der(CString) cstr(bool value) { return ifelse(value, vstr("true", 4), vstr("false", 5)); }
-
-	// 将char 转换为 CString 对象
-	der(CString) cstr(char value) { return dstr(&value, 1); }
-
-	// 将const char* 转换为 CString 对象
-	der(CString) cstr(const char* value, c_size len = -1)
-	{
-		if (len == -1) len = strlen(value);
-		return dstr(value, len);
-	}
-
-	// 将std::string 转换为 CString 对象
-	der(CString) cstr(const std::string& value) { return dstr(value); }
-
-	// 将std::string_view 转换为 CString 对象
-	der(CString) cstr(const std::string_view& value) { return dstr(value); }
-
-	// 将任意类型转换为 CString 对象
-	template<typename T>
-	der(CString) cstr(const T& value)
-	{
-		if constexpr (hasmethod(T, __str__))
-			return value.__str__();
-		else
-		{
-			Buffer buffer;
-			buffer << value;
-			return from_buffer(std::move(buffer));
-		}
 	}
 
 	Buffer& operator<< (Buffer& buffer, const CString& value)
